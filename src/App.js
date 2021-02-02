0beae018aae0529126ed99c14cccec9b9ad29a18
danrleylfd <danrleylfd@gmail.com>
2021-02-02 13:48:31 -0300

    FULL DOPAMINA

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header'
 import Routes from './routes'
 
 function App() {
-  console.log(process.env.API_URL)
+  console.log(process.env)
   return (
     <BrowserRouter>
       <Header />
--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -21,7 +21,7 @@ class Feed extends Component {
   }
 
   registerToSocket = () => {
-    const socket = io(process.env.API_URL)
+    const socket = io(process.env.REACT_APP_API_URL)
 
     socket.on('post', newPost => {
       this.setState({ feed: [newPost, ...this.state.feed] })
@@ -52,7 +52,7 @@ class Feed extends Component {
                 </div>
                 <img src={more} alt="Mais" />
               </header>
-              <img src={`${process.env.API_URL}/files/${post.image}`} alt="" />
+              <img src={`${process.env.REACT_APP_API_URL}/files/${post.image}`} alt="" />
             <footer>
               <div className="actions">
                 <button type="button" onClick={() => this.handleLike(post._id)}>
